9bcc4831ece733a561cf880cbcf0bbd515189cc2
Surajit  Dey <surajit@Macintosh.local>
2011-09-29 14:30:14 +0530

    [FIRST COMMIT]Scaffold of book and handlebars.js added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HandlebarWithRails::Application.config.secret_token = '3403f8cd89f12bc423a3f7924e69722e6ada2454c2c424936e1f754653a3ae648b6aa58d5100b9af4f6e06722e2f70ddfe7583cb0ae6f09a50150b3391689b78'
